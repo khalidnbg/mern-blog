b63b121b09ee4878b22d9891c209fd9f5e58fd14
Khalid Nabgaoui <khalidnabgaoui4@gmail.com>
2024-03-07 01:03:25 +0100

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All Fields Required" });
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.json({ message: "Signup Success" });
+  } catch (error) {
+    res.status(404).json({ message: "Signup Error : " + error.message });
+  }
+};
